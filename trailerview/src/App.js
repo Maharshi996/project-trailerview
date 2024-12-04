e24364517d4d4aff82100cd071b4ee32f480fd9c
Maharshi996 <raghavmanubrolu.9966@gmail.com>
2024-12-04 23:38:31 +0530

    integrating tailwind.css for styling

--- trailerview/src/App.js
+++ trailerview/src/App.js
@@ -1,9 +1,10 @@
+import "./index.css"
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-      <h1>Hello Maharshi</h1>
+      <h1 class="text-2xl font-bold underline p-96" >Hello Maharshi</h1>
       </header>
     </div>
   );
